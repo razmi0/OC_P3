d9d327aad1ef174aaed6190226a77a41f9064446
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-07 01:51:17 +0200

    Add imports, data retrieval, organization, filters creation, listeners attachment, and works display in main.js

---
+++ client/scripts/main.js
@@ -0,0 +1,23 @@
+//@ts-check
+import { getWorks, getCategories } from "./fetch.js";
+import { createGroupedWorks, attachListeners, createFilters, displayWorks } from "./script.js";
+
+const gallery = document.querySelector("#gallery");
+if (!gallery) throw new Error("No gallery found");
+const filters = document.querySelector("#filtersSection");
+if (!filters) throw new Error("No filters found");
+
+// Récupérer les données
+// --
+const worksData = await getWorks();
+const categoriesData = await getCategories();
+// Organiser les données
+// --
+const groupedWorks = createGroupedWorks(categoriesData, worksData);
+// Creer les filtres et poser les listeners
+// --
+const allFiltersElements = createFilters(filters, categoriesData);
+attachListeners(gallery, groupedWorks, allFiltersElements);
+// Afficher les works
+// --
+displayWorks(gallery, groupedWorks, "0");
