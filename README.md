cceb5ac4d5b10684b63e744916c309e0f7c9d6c8
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-06 13:52:06 +0200

    first commit

--- README.md
+++ README.md
@@ -4,5 +4,40 @@ Code du projet 6 d'intégrateur web.
 
 ## Information pour le lancer le code
 
- - Lancer le backend depuis votre terminal en suivant les instruction du fichier ReadMe.
- - Si vous désirez afficher le code du backend et du frontend, faites le dans 2 instances de VSCode différentes pour éviter tout problème
+- Lancer le backend depuis votre terminal en suivant les instruction du fichier ReadMe.
+- Si vous désirez afficher le code du backend et du frontend, faites le dans 2 instances de VSCode différentes pour éviter tout problème
+
+## Ce que Thomas à fait :
+
+### [**VITE**](https://vitejs.dev/)
+
+  <p>Installation d'un environnement de développement front-end profitant des avantages des navigateurs d'aujourd'hui, moderne, simple et extrêmement rapide . <br /> <i> Dossier /client/ </i>
+
+**Première visite :**
+
+```bash
+  $ cd client
+  $ npm install
+  $ npm run dev
+```
+
+**sinon juste**
+
+```bash
+  $ npm run dev
+```
+
+[LOCALHOST](http://localhost:5173) par défaut sur port 5173
+
+</p>
+<hr />
+
+### Styles
+
+- J'ai bougé les images dans le dossier ./public qui est déservit tel quel par le serveur de développement et de production
+- J'ai déplacé assets dans ./src pour y mettre les fichiers de styles => ./src/styles
+- J'ai rajouté une feuille de style : normalize.css [DOCUMENTATION](https://grafikart.fr/tutoriels/reset-normalize-css-1096)
+
+### Structure projet
+
+- /client/ remplace /FrontEnd/
---
+++ client/script-thomas.js
@@ -0,0 +1,96 @@
+//@ts-check
+
+const worksURL = "http://localhost:5678/api/works";
+const categoriesURL = "http://localhost:5678/api/categories";
+const selectGallery = document.querySelector("#gallery");
+const filters = document.querySelector("#filtersSection");
+let filteredWorks;
+
+const getWorks = async () => {
+  const response = await fetch(worksURL);
+  return await response.json();
+};
+
+const getCategories = async () => {
+  const response = await fetch(categoriesURL);
+  return await response.json();
+};
+
+// afficher wokrs
+const displayWorks = (worksData) => {
+  selectGallery.innerHTML = "";
+
+  worksData.forEach((work) => {
+    const elementsWork = document.createElement("figure");
+    const caption = document.createElement("figcaption");
+    const img = document.createElement("img");
+
+    img.src = work.imageUrl;
+    img.textContent = work.title;
+    caption.textContent = work.title;
+    elementsWork.setAttribute("data-work-id", work.id);
+
+    elementsWork.appendChild(img);
+    elementsWork.appendChild(caption);
+    selectGallery.appendChild(elementsWork);
+  });
+};
+
+// au click ...
+const click = (works) => {
+  filters.addEventListener("click", (event) => {
+    const selectedCategoryId = event.target.getAttribute("data-categories-id"); // Récupérer l'ID de la catégorie sélectionnée
+    console.log(selectedCategoryId);
+
+    if (selectedCategoryId !== "all") {
+      //si le click et diff de all on filtre sinon on garde les works
+      const filteredWorks = works.filter((work) => work.categoryId === selectedCategoryId);
+      displayWorks(filteredWorks);
+    } else {
+      displayWorks(works);
+    }
+  });
+};
+
+// afficher categories
+const displayCategories = (categoriesData, works) => {
+  const filtersUl = document.createElement("ul");
+  filtersUl.classList.add("categories");
+
+  filters.appendChild(filtersUl);
+
+  createFilters(categoriesData, filtersUl, works);
+};
+
+//creer les filtres
+const createFilters = (categoriesData, filter) => {
+  const allWorksElements = document.createElement("li");
+
+  allWorksElements.textContent = "Tous";
+  allWorksElements.classList.add("filters");
+
+  filter.appendChild(allWorksElements);
+
+  categoriesData.forEach((category) => {
+    const elementsList = document.createElement("li");
+
+    elementsList.textContent = category.name;
+    elementsList.setAttribute("data-categories-id", category.id);
+
+    elementsList.classList.add("filters");
+
+    filter.appendChild(elementsList);
+  });
+};
+
+//appeler les fonctions
+const worksData = await getWorks();
+console.log(worksData);
+
+const categoriesData = await getCategories();
+console.log(categoriesData);
+
+displayWorks(worksData);
+displayCategories(categoriesData, worksData);
+
+export {};
